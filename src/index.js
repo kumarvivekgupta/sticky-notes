dc567b896ed2f508d1e7b4a43701e2b37c982094
vivekMove1997 <vivekkumar.gupta@moveinsync.com>
2024-07-01 03:12:52 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <App />
+</React.StrictMode>
 );
 
 

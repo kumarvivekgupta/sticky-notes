91e4efaca6bbe7c8da66f31070f92091d5637bdb
vivekMove1997 <vivekkumar.gupta@moveinsync.com>
2024-07-01 09:55:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import HomeApp from "./components/HomeApp";
 import NotesApp from "./components/NotesApp";
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import OuterLayout from "./components/OuterLayout";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<OuterLayout />}>
+        <Route path='' element={<HomeApp />} />
+        <Route path='notes' element={<NotesApp />} />
+        
+      </Route>
+    )
+  )
 
   return (
     <>
       <div className="App">
-        <BrowserRouter >
-          <Routes>
-            <Route exact path="/" element={<HomeApp />} />
-            <Route path="/notes" element={<NotesApp />} />
-          </Routes>
-        </BrowserRouter>
-
-
+        <RouterProvider router={router} />
       </div>
     </>
   );
